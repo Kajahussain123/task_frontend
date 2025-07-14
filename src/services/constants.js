46cc561ae92c5490debabe0ddcf3a1107f9dc393
Kajahussain123 <kajahusain513@gmail.com>
2025-07-14 13:20:47 +0530

    updates

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL="http://localhost:3005/api"
+export const BASE_URL="https://task-backend-21ob.onrender.com/api"
+// export const BASE_URL="http://localhost:3005/api"
+
 
 export const API_ENDPOINTS = {
     USER_AUTH: '/auth',
